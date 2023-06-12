58ed5f615d7596e1f8f74bb6d3dfd0651ea35eda
Gabriel Gutierrez <97479321+GaboProgramers@users.noreply.github.com>
2023-06-12 13:03:23 -0500

    v0.3 sistema responsivo activo todas las medidas configuradas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import HeaderNav from './components/headers/HeaderNav'
+import { useState } from 'react';
+import './App.css';
+import HeaderNav from './components/headers/HeaderNav';
 
 function App() {
-
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <main className={`App ${!darkMode ? '' : 'dark'}`}>
       <div>
-        <HeaderNav
-          darkMode={darkMode}
-          setDarkMode={setDarkMode}
-        />
+        <HeaderNav darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
